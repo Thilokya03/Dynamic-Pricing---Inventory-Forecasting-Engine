1e42b217fd90322ebd125ba44604883fc3e69055
Thilokya Angeesa <thilokyaangeesa@gmail.com>
2026-08-15 16:55:29 +0530

    add silver data pipeline

--- scripts/1.create_silver_dataset.py
+++ scripts/1.create_silver_dataset.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-import pandas as pd
-import os
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -10,9 +10,10 @@ RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
 
 SILVER_DATA_DIR = PROJECT_ROOT / "data" / "silver"
 
-def transform_sales_data(sales_df: pd.DataFrame) -> pd.DataFrame:
+def transform_sales_data(sales_df: DataFrame) -> DataFrame:
     # Product and location columns to keep
     id_columns = [
+        'id',
         "item_id",
         "dept_id",
         "cat_id",
@@ -27,66 +28,83 @@ def transform_sales_data(sales_df: pd.DataFrame) -> pd.DataFrame:
         if column.startswith("d_")
     ]
 
-    long_df = sales_df.melt(
-        id_vars=id_columns,
-        value_vars=day_columns,
-        var_name="d",
-        value_name="sales",
+    sales_df = sales_df.unpivot(
+        ids = id_columns,
+        values = day_columns,
+        variableColumnName = "d",
+        valueColumnName = "sales"
     )
+    return sales_df
 
-    return long_df
 
-
-def add_calendar_info(sales_df: pd.DataFrame, calendar_df: pd.DataFrame) -> pd.DataFrame:
-
-    result_df = sales_df.merge(
-        calendar_df[["d", "date", "wm_yr_wk"]],
-        on="d",
-        how="left"
+def add_calendar_info(sales_df: DataFrame, calendar_df: DataFrame) -> DataFrame:
+    result_df = sales_df.join(
+        calendar_df,
+        on = ['d'],
+        how = 'left'
+        
     )
-    result_df["date"] = pd.to_datetime(result_df["date"])
-
     return result_df
 
 
-def add_sales_info(sales_df: pd.DataFrame, sell_prices_df: pd.DataFrame) -> pd.DataFrame:
-
-    result_df = sales_df.merge(
+def add_price_info(sales_df: DataFrame, sell_prices_df: DataFrame) -> DataFrame:
+    result_df = sales_df.join(
         sell_prices_df,
-        on=["store_id", "item_id", "wm_yr_wk"],
-        how="left"
+        on = ['store_id','item_id', 'wm_yr_wk'],
+        how = 'left'
     )
+    
 
     return result_df
 
-def save_to_csv(df: pd.DataFrame, filename: str):
+def save_to_parquet(df: DataFrame, filename: str):
     SILVER_DATA_DIR.mkdir(parents=True, exist_ok=True)
-    df.to_csv(SILVER_DATA_DIR / filename, index=False)
-     
+    output_path = SILVER_DATA_DIR / filename
+    df.write.mode('overwrite').partitionBy('year').parquet(str(output_path))
 
-if __name__ == "__main__":
+
+def main():
+    
+    spark = SparkSession.builder.master("local[*]").appName("SilverDataset").getOrCreate()
+    print("Spark session created.")
+    
+    print("--" * 20)
     print("Loading raw data...")
-    calendar_df = pd.read_csv(RAW_DATA_DIR / "calendar.csv")
-    sales_evaluation_df = pd.read_csv(RAW_DATA_DIR / "sales_train_evaluation.csv")
-    sales_validation_df = pd.read_csv(RAW_DATA_DIR / "sales_train_validation.csv")
-    sell_prices_df = pd.read_csv(RAW_DATA_DIR / "sell_prices.csv")
+    
+    print("--" * 20)
+    
+    print("Loading raw data...")
+    calendar_df = spark.read.option("header", "true").csv(str(RAW_DATA_DIR / "calendar.csv"), inferSchema=True)
+    sales_eval_df = spark.read.option("header", "true").csv(str(RAW_DATA_DIR / "sales_train_evaluation.csv"), inferSchema=True)
+
+    prices_df = spark.read.option("header", "true").csv(str(RAW_DATA_DIR / "sell_prices.csv"), inferSchema=True)
+    print("Raw data loaded successfully.")
+    
+    print("--" * 20)
     
     print("Transforming sales data...")
-    new_sales_validation_df = transform_sales_data(sales_validation_df)
-    new_sales_evaluation_df = transform_sales_data(sales_evaluation_df)
+    new_sales_eval_df = transform_sales_data(sales_eval_df)
+
     
     print("Adding calendar info...")
-    new_sales_validation_df = add_calendar_info(new_sales_validation_df, calendar_df)
-    new_sales_evaluation_df = add_calendar_info(new_sales_evaluation_df, calendar_df)
+    new_sales_eval_df = add_calendar_info(new_sales_eval_df, calendar_df)
     
-    print("Adding sales info...")
-    new_sales_validation_df = add_sales_info(new_sales_validation_df, sell_prices_df)
-    new_sales_evaluation_df = add_sales_info(new_sales_evaluation_df, sell_prices_df)
+    print("Adding price info...")
+    new_sales_eval_df = add_price_info(new_sales_eval_df, prices_df)
+    
+    print("--" * 20)
     
     print("Saving transformed data to silver dataset...")
-    save_to_csv(new_sales_validation_df, "new_sales_validation.csv")
-    save_to_csv(new_sales_evaluation_df, "new_sales_evaluation.csv")
-    
+    save_to_parquet(new_sales_eval_df, "sales")
+
     print("Silver dataset created successfully at:", SILVER_DATA_DIR)
     
+    print("--" * 20)
+    spark.stop()
+    print("Spark session stopped.")
     
+
+
+if __name__ == "__main__":
+    main()
+
