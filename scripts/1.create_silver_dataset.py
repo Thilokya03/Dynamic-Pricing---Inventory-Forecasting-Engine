63593d7b0df5e4975863874a6e32645e1aa3b100
Thilokya Angeesa <thilokyaangeesa@gmail.com>
2026-08-16 11:03:28 +0530

    update the workflow and use polar to create silver dataset

--- scripts/1.create_silver_dataset.py
+++ scripts/1.create_silver_dataset.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-from pyspark.sql import DataFrame
-from pyspark.sql import SparkSession
+import polars as pl
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -10,7 +9,7 @@ RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw"
 
 SILVER_DATA_DIR = PROJECT_ROOT / "data" / "silver"
 
-def transform_sales_data(sales_df: DataFrame) -> DataFrame:
+def transform_sales_data(sales_df: pl.LazyFrame) -> pl.LazyFrame:
     # Product and location columns to keep
     id_columns = [
         'id',
@@ -28,16 +27,16 @@ def transform_sales_data(sales_df: DataFrame) -> DataFrame:
         if column.startswith("d_")
     ]
 
-    sales_df = sales_df.unpivot(
-        ids = id_columns,
-        values = day_columns,
-        variableColumnName = "d",
-        valueColumnName = "sales"
+    sales_df = sales_df.melt(
+        id_vars=id_columns,
+        value_vars=day_columns,
+        variable_name="d",
+        value_name="sales"
     )
     return sales_df
 
 
-def add_calendar_info(sales_df: DataFrame, calendar_df: DataFrame) -> DataFrame:
+def add_calendar_info(sales_df: pl.LazyFrame, calendar_df: pl.LazyFrame) -> pl.LazyFrame:
     result_df = sales_df.join(
         calendar_df,
         on = ['d'],
@@ -47,37 +46,36 @@ def add_calendar_info(sales_df: DataFrame, calendar_df: DataFrame) -> DataFrame:
     return result_df
 
 
-def add_price_info(sales_df: DataFrame, sell_prices_df: DataFrame) -> DataFrame:
+def add_prices_info(sales_df: pl.LazyFrame, prices_df: pl.LazyFrame) -> pl.LazyFrame:
     result_df = sales_df.join(
-        sell_prices_df,
+        prices_df,
         on = ['store_id','item_id', 'wm_yr_wk'],
         how = 'left'
     )
-    
-
     return result_df
 
-def save_to_parquet(df: DataFrame, filename: str):
-    SILVER_DATA_DIR.mkdir(parents=True, exist_ok=True)
-    output_path = SILVER_DATA_DIR / filename
-    df.write.mode('overwrite').partitionBy('year').parquet(str(output_path))
 
+def save_to_parquet(df: pl.LazyFrame, filename: str):
+    SILVER_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    output_path = SILVER_DATA_DIR / f"{filename}.parquet"
+
+    df.sink_parquet(
+        output_path,
+        compression="snappy",
+        engine="streaming",
+    )
 
 def main():
     
-    spark = SparkSession.builder.master("local[*]").appName("SilverDataset").getOrCreate()
-    print("Spark session created.")
-    
-    print("--" * 20)
     print("Loading raw data...")
     
     print("--" * 20)
     
     print("Loading raw data...")
-    calendar_df = spark.read.option("header", "true").csv(str(RAW_DATA_DIR / "calendar.csv"), inferSchema=True)
-    sales_eval_df = spark.read.option("header", "true").csv(str(RAW_DATA_DIR / "sales_train_evaluation.csv"), inferSchema=True)
-
-    prices_df = spark.read.option("header", "true").csv(str(RAW_DATA_DIR / "sell_prices.csv"), inferSchema=True)
+    calendar_df = pl.scan_csv(str(RAW_DATA_DIR / "calendar.csv"))
+    sales_eval_df = pl.scan_csv(str(RAW_DATA_DIR / "sales_train_evaluation.csv"))
+    prices_df = pl.scan_csv(str(RAW_DATA_DIR / "sell_prices.csv"))
     print("Raw data loaded successfully.")
     
     print("--" * 20)
@@ -90,18 +88,19 @@ def main():
     new_sales_eval_df = add_calendar_info(new_sales_eval_df, calendar_df)
     
     print("Adding price info...")
-    new_sales_eval_df = add_price_info(new_sales_eval_df, prices_df)
+    new_sales_eval_df = add_prices_info(new_sales_eval_df, prices_df)
     
     print("--" * 20)
     
     print("Saving transformed data to silver dataset...")
-    save_to_parquet(new_sales_eval_df, "sales")
+    save_to_parquet(
+        new_sales_eval_df,
+        "sales"
+        )
 
     print("Silver dataset created successfully at:", SILVER_DATA_DIR)
     
-    print("--" * 20)
-    spark.stop()
-    print("Spark session stopped.")
+
     
 
 
